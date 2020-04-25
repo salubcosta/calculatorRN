799fa3d77462f053f45fcb1f784df191dada6042
salubcosta <salubcosta@gmail.com>
2020-04-25 01:00:28 -0300

    Calculator project done. A simple calculator created with purpose study and to practice what i am learning

--- src/App.js
+++ src/App.js
@@ -16,18 +16,19 @@ const App = ()=>{
 
     const [displayValue, setDisplayValue] = useState('0');
     const [clearDisplay, setClearDisplay] = useState(false);
-    const [oparation, setOp] = useState(null);
+    const [op, setOp] = useState(null);
     const [values, setValues] = useState([0,0]);
     const [current, setCurrent] = useState(0);
 
-    const addDigit = n =>{
-        // Esta validação não deixa incluir mais de 1 ponto. Ex: 84.5.2
-        if(n ==='.' && displayValue.includes('.')){
-            return
-        }
-        
+    const addDigit = n =>{        
         // Esta validação faz com que não seja incluído 0 à esquerda. Ex: 00086.2 
         const _clearDisplay = displayValue === '0' || clearDisplay;
+
+         // Esta validação não deixa incluir mais de 1 ponto. Ex: 84.5.2
+        if(n ==='.' && !_clearDisplay && displayValue.includes('.')){
+            return
+        }
+
         const _current = _clearDisplay ? '' : displayValue;
         const _displayValue = _current + n;
 
@@ -38,7 +39,7 @@ const App = ()=>{
             const _newValue = parseFloat(_displayValue);
             const _values = values;
             _values[current] = _newValue;
-            setValues({_values})
+            setValues(_values)
         }
     }
 
@@ -47,7 +48,33 @@ const App = ()=>{
     }
 
     const setOperation = operation =>{
-        alert(operation)
+        if(current===0){
+            setOp(operation);
+            setCurrent(1);
+            setClearDisplay(true);
+        } else {
+            const equals = operation === '=';
+            const _values = values;
+            try{
+                // A função eval faz isso: eval('1 + 1') resultar em 2
+                _values[0] = eval(`${_values[0]} ${op} ${_values[1]}`);
+            }catch(e){
+                _values[0] = values[0];
+            }
+
+            _values[1] = 0;
+            setDisplayValue(`${_values[0]}`);
+            
+            _setOp = equals ? null : operation;
+            setOp(_setOp);
+
+            _setCurrent = equals ? 0 : 1;
+            setCurrent(_setCurrent);
+            
+            setClearDisplay(!equals);
+
+            setValues(_values);
+        }
     }
 
     return(
